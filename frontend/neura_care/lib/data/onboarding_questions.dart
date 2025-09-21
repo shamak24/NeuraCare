8e02faa2522fb008b580584494535fe41e127eae
Gurkirat Singh Dhillon <navilavi.kol123@gmail.com>
2025-09-21 10:43:58 +0530

    update

--- frontend/neura_care/lib/data/onboarding_questions.dart
+++ frontend/neura_care/lib/data/onboarding_questions.dart
@@ -1,30 +1,30 @@
 import 'package:neura_care/models/question.dart';
 
 final List<Question> onboardingQuestions = [
-  Question(text: 'What is your age?', type: QuestionType.int, options: []),
+  Question(text: 'What is your age? (in years)', type: QuestionType.int, options: []),
   Question(
     text: 'What is your gender?',
     type: QuestionType.choice,
     options: ['Male', 'Female', 'Other'],
   ),
   Question(
-    text: "What is your Systolic blood pressure?",
+    text: "What is your Systolic blood pressure? (in mmHg)",
     type: QuestionType.int,
   ),
   Question(
-    text: "What is your Diastolic blood pressure?",
+    text: "What is your Diastolic blood pressure? (in mmHg)",
     type: QuestionType.int,
   ),
-  Question(text: "What is your heartRate", type: QuestionType.int),
-  Question(text: "What is your sugarLevel", type: QuestionType.int),
-  Question(text: "What is your weight", type: QuestionType.double),
-  Question(text: "What is your cholesterol", type: QuestionType.int),
+  Question(text: "What is your heartRate? (in bpm)", type: QuestionType.int),
+  Question(text: "What is your Blood sugar level? (in mg/dL)", type: QuestionType.int),
+  Question(text: "What is your weight? (in kg)", type: QuestionType.double),
+  Question(text: "What is your cholesterol? (in mg/dL)", type: QuestionType.int),
   Question(
     text: 'What is your activity level?',
     type: QuestionType.choice,
     options: ["Sedentary", "Lightly Active", "Active", "Very Active"],
   ),
-  Question(text: "What is your height", type: QuestionType.double),
+  Question(text: "What is your height? (in cm)", type: QuestionType.double),
   Question(
     text: "Do you smoke?",
     type: QuestionType.choice,
@@ -35,5 +35,5 @@ final List<Question> onboardingQuestions = [
     type: QuestionType.choice,
     options: ["false", "true"],
   ),
-  Question(text: "How many hours do you sleep?", type: QuestionType.double),
+  Question(text: "How many hours do you sleep? (in hours)", type: QuestionType.double),
 ];
